c895451c86b7d86d9b572c46239dba23776a7f1e
Pinae <jonny@pinae.net>
2017-08-29 21:46:37 +0200

    Cleaner code

--- mouse.py
+++ mouse.py
@@ -1,7 +1,14 @@
 from Xlib import display
-from time import sleep
-while True:
+
+
+def get_mouse_position():
     data = display.Display().screen().root.query_pointer()._data
-    print(str((data["root_x"], data["root_y"])) + "        \r", end="")
-    sleep(0.05)
+    return data["root_x"], data["root_y"]
+
+
+if __name__ == "__main__":
+    from time import sleep
+    while True:
+        print("{}        \r".format(get_mouse_position()), end="")
+        sleep(0.05)
 
