3eaa76445a56983843d55c05b4a6bf36227be5a4
Pinae <jonny@pinae.net>
2017-08-29 23:01:58 +0200

    Get list of open windows (working this time)

--- open_windows.py
+++ open_windows.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from Xlib import display, X
+from subprocess import PIPE, Popen
+import re
+wlp = re.compile(b"^(0x[\da-f]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(.+)$")
 
 
 def get_list_of_open_windows():
-    x_win_list = display.Display().screen().root.query_tree().children
-    window_list = []
-    for win in x_win_list:
-        try:
-        #if win.get_attributes().map_state == X.IsViewable:
-            name = win.get_wm_name()
-            geometry = win.get_geometry()
-            window_list.append((name, {'x': geometry.x, 'y': geometry.y,
-                                       'width': geometry.width, 'height': geometry.height}))
-        except TypeError:
-            print(win.id)
-    return window_list
+    win_list = []
+    root = Popen(['wmctrl', '-p', '-G', '-l'], stdout=PIPE)
+    stdout, stderr = root.communicate()
+    lines = stdout.split(b"\n")
+    for line in lines:
+        matches = wlp.match(line)
+        if matches:
+            win_id, destop_no, pid, x, y, width, height, name = matches.groups()
+            win_list.append({
+                'name': name,
+                'id': win_id,
+                'x': int(x), 'y': int(y),
+                'width': int(width), 'height': int(height),
+                'destop_no': int(destop_no),
+                'pid': int(pid)
+            })
+    return win_list
+
+
+def position_window(win_id, x, y, width, height):
+    Popen(['wmctrl', '-i', '-r', win_id,  '-e', ','.join([x, y, width, height])])
+
+
+def focus_window(win_id):
+    Popen(['wmctrl', '-i', '-a', win_id])
 
 
 if __name__ == "__main__":
